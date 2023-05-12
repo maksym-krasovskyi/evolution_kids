d41c706f469ac010b567a675ea29aeef7bb0580b
Dmytro <dimores95@gmail.com>
2023-05-12 14:32:45 +0300

    added fix button request coll

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,8 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+document.querySelector('.request-call__button')?.addEventListener('click', function(e) {
+    document.querySelector('.request-call')?.classList.toggle('_show');
+});
+
